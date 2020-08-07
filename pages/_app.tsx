61926465c974431c29267b21bad42e515a42ab09
Charles Tison <ctison@pm.me>
2020-08-07 21:08:45 +0200

    Clean import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import { AppProps } from 'next/app'
 import { CssBaseline } from '@material-ui/core'
 import Head from 'next/head'
-import Header from '@/components/Header'
+import { Header } from '@/components'
 import { SWRConfig } from 'swr'
 import { ApolloClient, HttpLink, InMemoryCache, ApolloProvider } from '@apollo/client'
 
